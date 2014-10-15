ad00b6411776c336f50fdf3752221ac543dcb948
Paul Craciunoiu <paul@craciunoiu.net>
2014-10-15 17:30:12 -0600

    Restore static path. Relocate some stuff.

--- pelican.conf.py
+++ pelican.conf.py
@@ -52,7 +52,7 @@ THEME = './themes/embrangler/'
 MARKUP = 'md'
 MD_EXTENSIONS = ['headerid', 'codehilite']
 
-STATIC_PATHS = ['images', 'files']
+STATIC_PATHS = ['static']
 
 # default value is ('index', 'tags', 'categories', 'archives')
 DIRECT_TEMPLATES = ('index', 'tags', 'sitemap')
