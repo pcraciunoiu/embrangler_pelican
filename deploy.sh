df0fcd07553101ae0db2217064a53b53de6351e2
Paul Craciunoiu <paul@craciunoiu.net>
2019-12-29 12:29:16 -0700

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pipenv run make html
-# TODO: figure out how to chown/fix post-deploy static issues
-# sudo chown -R uplift:www-data output/
+/usr/local/bin/pipenv run make html
+# This must run as the `uplift` user, so as not to require sudo
+chown -R uplift:www-data output/
